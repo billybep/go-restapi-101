f32b027a3eaee3a5a69ab41d37d56acecf8b0307
billybep <billy.pesoth@gmail.com>
2021-11-23 20:09:09 +0700

    test add get request

--- main.go
+++ main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+// Create Object TODO
+type ToDo struct {
+	Activity string `json:"activity"`
+	Time string `json:"waktu"`
+}
+
+type JSONResponse struct {
+	Code int `json:"code"`
+	Success bool `json:"success"`
+	Message string `json:"message"`
+	Data []ToDo `json:"data"`
+}
 
 func main() {
-	fmt.Println("Hello World")
+
+	// create endpoint
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// GET /
+		if r.Method == "GET" {
+			rw.Header().Add("Content-Type", "application/json")
+
+			res := JSONResponse{
+				http.StatusOK,
+				true,
+				"Testing Get Method",
+				[]ToDo{},
+			}
+
+			// convert res to json
+			dataJSON, err := json.Marshal(res)
+
+			if err != nil {
+				http.Error(rw, "Something wrong", http.StatusInternalServerError)
+			}
+
+			rw.Write(dataJSON)
+
+		} else if r.Method == "POST" {
+
+		}
+
+	})
+
+	fmt.Println("Listning on port: 8080 ....")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
